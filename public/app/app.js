3440965972062c499a64c32299dc68976c8a70b1
PBDesk Admin <pbdesk@pbdesk.com>
2014-09-24 02:11:14 +0000

    ready for first commit

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,10 @@
 angular.module('app',[]);
 
 angular.module('app').controller('testCtrl', function($scope){
-     $scope.test = 'ng binding is working';
-
+     //$scope.test = 'ng binding is working';
+     $scope.contacts = [
+          {name: 'xyz abc', email: 'a@a.com'},
+          {name: 'pqr uvw', email: 'p@p.com'},
+          {name: 'ooo bcd', email: 'q@q.com'}
+          ];
 });
