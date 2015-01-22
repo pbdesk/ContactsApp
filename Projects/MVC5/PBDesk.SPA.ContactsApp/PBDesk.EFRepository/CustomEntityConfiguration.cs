b6f498c1b649502416acca5f00f85312be623ad1
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2015-01-22 17:24:59 -0500

    EFRepository and ngApp common files

---
+++ Projects/MVC5/PBDesk.SPA.ContactsApp/PBDesk.EFRepository/CustomEntityConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBDesk.EFRepository
+{
+    public class CustomEntityConfiguration<T> : EntityTypeConfiguration<T>     where T:  Entity
+    {
+        public CustomEntityConfiguration()
+        {
+            init(); 
+        }
+
+        public CustomEntityConfiguration(string tableName, string pkFieldName = "")
+        {
+            if (!string.IsNullOrWhiteSpace(tableName))
+            {
+                ToTable(tableName);
+                init(pkFieldName);
+            }
+            else
+            {
+                throw new Exception("tableName cannot be null or empty");
+            }
+        }
+
+        private void init(string pkFieldName  = "")
+        {
+            if(string.IsNullOrWhiteSpace(pkFieldName))
+            {
+                pkFieldName = "Id";
+            }
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasColumnName(pkFieldName).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(x => x.IsActive).HasColumnName("IsActive"); ;
+            Property(x => x.LastUpdBy).HasColumnName("LastUpdBy").HasMaxLength(256);
+            Property(x => x.LastUpdDate).HasColumnName("LastUpdDate");
+        }
+    }
+}
---
+++ Projects/MVC5/PBDesk.SPA.ContactsApp/PBDesk.SPA.ContactsApp.Web/ngApp/core/PBDesk.Contacts.core.module.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('PBDesk.Contacts.Core', [
+        'ngAnimate', 'ngRoute', 'ngSanitize',
+        'blocks.exception', 'blocks.logger', 'blocks.router',
+        'GoogleFeedsApp',
+        'angular-data.DSCacheFactory'
+    ]);
+})();
