30eb16c1b471920de762e1703279516c0d40650f
PinalBhattLive <Pinal-Bhatt@live.com>
2015-01-22 20:36:54 -0500

    Contacts Controller and DBContext created

---
+++ Projects/MVC5/PBDesk.SPA.ContactsApp/PBDesk.SPA.ContactsApp.Web/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PBDesk.SPA.ContactsApp.Web.Models
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+	}
+}
