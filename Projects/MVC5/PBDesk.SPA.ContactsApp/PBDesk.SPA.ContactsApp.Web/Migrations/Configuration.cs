a75693bd42f81e7bd339b726f5d71518e0de699b
PinalBhattLive <Pinal-Bhatt@live.com>
2015-01-22 20:37:14 -0500

    EF Migrations enabled

---
+++ Projects/MVC5/PBDesk.SPA.ContactsApp/PBDesk.SPA.ContactsApp.Web/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace PBDesk.SPA.ContactsApp.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PBDesk.SPA.ContactsApp.Web.Models.ContactsAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PBDesk.SPA.ContactsApp.Web.Models.ContactsAppContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
